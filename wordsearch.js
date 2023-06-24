3bb53900ca4e0dff879fa0b9703d43ad06b55469
Vicky John <vicky.d.john@email.com>
2023-06-23 17:06:48 -0700

    added functionality for vertical checks and added various test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (word === "") {
+        return false;
     }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
 
-module.exports = wordSearch
+    for (let i = 0; i < letters.length; i++) {
+        let result = '';
+        for (let j = 0; j < letters[i].length; j++) {
+            result += letters[j][i];
+        }
+        if (result.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+module.exports = wordSearch;
